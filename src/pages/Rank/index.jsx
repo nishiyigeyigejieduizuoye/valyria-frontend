15c8c86b6521842d3c4bc74e47714f7529cf443c
veoxl <2581216@qq.com>
2022-11-01 11:44:28 +0800

    new rank page

--- src/pages/Rank/index.jsx
+++ src/pages/Rank/index.jsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
 import RankList from './Compoent/RankList';
-
-import { Box } from "@mui/material";
-
 const Rank = () => {
-    const navigate = useNavigate();
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <RankList />
-        </Box >
-
+        <RankList />
     );
 }
 export default Rank;
