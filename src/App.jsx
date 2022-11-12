fbaaab84374fd04be6ae5e6f1875a1a5885237be
LacLic <281885421@qq.com>
2022-11-12 12:48:44 +0800

    rename api.js -> login_api.js and component/ -> components/

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
   useRecoilValue,
 } from 'recoil';
 import { CssBaseline } from "@mui/material";
-import SiteAppBar from './component/AppBar';
+import SiteAppBar from './components/AppBar';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home'
 import Battle from './pages/Battle'
@@ -17,7 +17,7 @@ import Login from './pages/Login'
 import History from './pages/History'
 import Rank from './pages/Rank'
 import { Container } from '@mui/system';
-import GlobalMessage from './component/GlobalMeaage';
+import GlobalMessage from './components/GlobalMeaage';
 
 function App() {
   return (
