3788df2f3609bb080e3aaadc8bc1a95642eab882
chenyuhao <2581216@qq.com>
2022-11-23 20:30:08 +0800

    complete change avatar

--- src/api/avatar_api.js
+++ src/api/avatar_api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
+  headers:{'Content-Type':'multipart/form-data'},
   timeout: 10000,
+  
 });
 export async function post_user_avatar(avatar) {
   return await instance.post("/api/user/avatar",{
-    headers:{'Content-Type':'multipart/form-data'},
     avatar:avatar,
   }
   );
